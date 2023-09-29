22f97767b80bb8db9c9ebcf37ec86d84e29ce686
micromashor <micromashor@gmail.com>
2023-09-28 19:42:47 -0500

    setup initial page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 style="text-align: center; font-size: 3em; color: white;">Multirotor Flight App</h1>
