2e4161d71210103e009dc1b35280937a68264eba
micromashor <micromashor@gmail.com>
2023-10-12 18:51:22 -0500

    get env var from dynamic vars

--- src/routes/api/weather/+server.ts
+++ src/routes/api/weather/+server.ts
@@ -1,12 +1,12 @@
 import type { RequestEvent, RequestHandler } from './$types'
-import { WEATHER_API_KEY } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export const POST: RequestHandler = async ({ request }: RequestEvent) => {
     try {
         const { location } = await request.json();
 
         const weather = await fetch(
-            `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${location}&aqi=no`
+            `https://api.weatherapi.com/v1/current.json?key=${env.WEATHER_API_KEY}&q=${location}&aqi=no`
         );
 
         return new Response(weather.body);
