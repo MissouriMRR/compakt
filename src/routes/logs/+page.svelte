890f31fd8e55b5d68eb7dcd854e045317c18a812
Luke <lmyszka44@gmail.com>
2023-10-17 18:38:40 -0500

    Text Box

---
+++ src/routes/logs/+page.svelte
@@ -0,0 +1,14 @@
+<div id="flightform">
+	<h2 style="text-align: center">Flight Logs</h2>
+</div>
+
+<style>
+	#flightform {
+		max-width: 100ch;
+		background-color: rgb(255, 255, 255);
+		padding: 1em;
+		border-radius: 1em;
+		margin: auto;
+		font-family: Proxima;
+	}
+</style>
