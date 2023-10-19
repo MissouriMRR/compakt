e8dcbdfd1dc4eca38f86360ae8882b65c370dfe4
tkbessler <tkbessler@gmail.com>
2023-10-19 18:09:01 -0500

    rename var

--- src/routes/logs/+page.svelte
+++ src/routes/logs/+page.svelte
@@ -1,10 +1,10 @@
-<div id="flightform">
+<div id="flightlogs">
 	<h2 style="text-align: center">Flight Logs</h2>
 	<h3 style="text-align: center">No logs to display</h3>
 </div>
 
 <style>
-	#flightform {
+	#flightlogs {
 		max-width: 100ch;
 		background-color: rgb(255, 255, 255);
 		padding: 1em;
