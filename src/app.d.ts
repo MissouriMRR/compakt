09c0111e27f9d7ce6b31e34f41ee51064316a8a8
micromashor <micromashor@gmail.com>
2023-10-19 18:36:45 -0500

    updates from the last week

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,7 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type Cloudflare from "@types/cloudflare";
+
 declare global {
     namespace App {
         // interface Error {}
@@ -9,6 +11,7 @@ declare global {
         interface Platform {
             env: {
                 WEATHER_API_KEY: string;
+                DB: Cloudflare.D1Database;
             };
             context: {
                 waitUntil(promise: Promise): void;
