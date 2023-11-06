85ddc8bb39e348a84b80b1b00cfb95d0deabd712
tkbessler <tkbessler@gmail.com>
2023-11-06 12:56:13 -0600

    refactored log recording

--- src/routes/record/+page.svelte
+++ src/routes/record/+page.svelte
@@ -1,22 +1,32 @@
 <script lang="ts">
-	interface WeatherData {}
+	const DEFAULT_LOC = 'Rolla, MO';
 
-	let resdata = '';
-	let weatherData = null as any;
-
-	const currentData = {
-		date: new Date().toDateString(),
-		location: 'Rolla, MO',
-		startTime: new Date().toISOString(),
-		stopTime: new Date().toISOString(),
-		tempF: '',
-		tempC: '',
-		windSpeed: '',
-		windDirection: '',
-		windDegree: '',
-		gustSpeed: '',
-		Humidity: '',
+	interface WeatherData {
+		condition: string,
+		icon: string,
+		temperatureF: Number,
+		temperatureC: Number,
+		windSpeedMPH: Number,
+		windDirection: string,
+		windDegree: Number,
+		gustSpeedMPH: Number,
+		humidity: Number
 	};
+
+	interface FlightData {
+		location: string,
+		flightDate: string,
+		flightStartTime?: string,
+		flightStopTime?: string,
+		weather?: WeatherData,
+		pilotID?: Number,
+		remoteID?: string
+	};
+
+	const flightData = {
+		location: 'Rolla',
+		flightDate: extractDate(new Date())
+	} as FlightData;
 
 	async function loadWeatherData() {
 		try {
@@ -24,23 +34,40 @@
 				method: 'POST',
 				body: JSON.stringify({ location })
 			});
+			
 			const data = await response.json();
-			resdata = JSON.stringify(data, undefined, 4);
+
+			if(data.error) {
+				console.error("[Weather API] API Error:", data.error.message);
+				return;
+			}
+			
+			flightData.weather = {
+				condition: data.current.condition.text,
+				icon: data.current.condition.icon,
+				temperatureF: data.current.temp_f,
+				temperatureC: data.current.temp_c,
+				windSpeedMPH: data.current.wind_mph,
+				windDirection: data.current.wind_dir,
+				windDegree: data.current.wind_degree,
+				gustSpeedMPH: data.current.gust_mph,
+				humidity: data.current.humidity
+			} as WeatherData;
 		} catch (error) {
-			console.error('Error fetching weather data:', error);
+			console.error("[Weather API]", error);
 		}
 	}
 
-	const pad = (num: Number) => num.toString().padStart(2, '0');
-
-	function getLocalTime() {
-		const now = new Date();
-		return `${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}`;
+	function pad(num: number) {
+		return num.toString().padStart(2, '0');
 	}
 
-	function getLocalDate() {
-		const now = new Date();
-		return `${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDate())}`;
+	function extractDate(date: Date) {
+		return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}`;
+	}
+
+	function extractTime(date: Date) {
+		return `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
 	}
 </script>
 
@@ -50,7 +77,7 @@
 
 	<form id="weatherautofill" style="text-align: center" on:submit={loadWeatherData}>
 		<label for="location">Enter your location to auto-fill some fields</label><br /><br />
-		<input id="location" type="text" value={currentData.location} />
+		<input id="location" type="text" value={flightData.location}/>
 		<input type="submit" value="Go" />
 	</form>
 
@@ -58,56 +85,56 @@
 		<h3 style="text-align: center;">Time & Date</h3>
 		<h4 style="text-align: center;">
 			<label for="date">Date</label>
-			<input type="date" id="date" value={currentData.date} />
+			<input type="date" id="date" value={flightData.flightDate} />
 			<button
 				on:click={() => {
-					currentData.date = getLocalDate();
+					flightData.flightDate = extractDate(new Date());
 				}}>Today</button
 			>
 		</h4>
 
 		<h4 style="text-align: center;">
 			<label for="t_start">Start Time</label>
-			<input type="time" id="t_start" value={currentData.startTime} step="1" />
+			<input type="time" id="t_start" value={flightData.flightStartTime || ''} step="1" />
 			<button
 				on:click={() => {
-					currentData.startTime = getLocalTime();
+					flightData.flightStartTime = extractTime(new Date());
 				}}>Now</button
 			>
 		</h4>
 
 		<h4 style="text-align: center;">
 			<label for="t_end">End Time</label>
-			<input type="time" id="t_end" value={currentData.stopTime} step="1" />
+			<input type="time" id="t_end" value={flightData.flightStopTime || ''} step="1" />
 			<button
 				on:click={() => {
-					currentData.stopTime = getLocalTime();
+					flightData.flightStopTime = extractTime(new Date());
 				}}>Now</button
 			>
 		</h4>
 		
+		{#if flightData.weather}
 		<h3 style="text-align: center;">Weather</h3>
 		<h4 style="text-align: center;">
 			<div>
 				<div class = 'sectionContainer'>
-					<p class = 'sectionTitle'>Location: {weatherData ? weatherData.location.name : ''}</p>
-					<p class = 'sectionTitle'>Region: {weatherData ? weatherData.location.region : ''}</p>
+					<p class = 'sectionTitle'>Location: {flightData.location}</p>
 				</div>
 
 				<div class = 'temperatureContainer'>
-					<p class = 'sectionTitle'>Temperature: {weatherData ? weatherData.current.temp_f + '°F' : ''}</p>
+					<p class = 'sectionTitle'>Temperature: {flightData.weather.temperatureF + '°F'}</p>
 				</div>
 				<div class = 'sectionRow'>
-					<p class = 'sectionTitle'>Condition: {weatherData ? weatherData.current.condition.text : ''}</p>
+					<span class = 'sectionTitle'>Condition: {flightData.weather.condition}</span>
 					<img
 						class='weatherIcon'
-						style='display: {weatherData ? "block" : "none"}'
-						src='https:${weatherData ? weatherData.current.condition.icon : ''}'
+						src={flightData.weather.icon}
 						alt='Weather Icon'
 					/>
 				</div>
 			</div>
 		</h4>
+		{/if}
 	</form>
 </div>
 
@@ -117,8 +144,10 @@
 		margin-top: 10;
   }
 	.sectionRow {
+		display: flex;
 		flex-direction: row;
 		align-items: center;
+		justify-content: center;
 	}
   .sectionTitle {
     font-size: 16;
@@ -131,8 +160,9 @@
     align-items: 'center';
   }
   .weatherIcon {
-    width: 30;
-    height: 30;
+		display: inline-block;
+    width: 2em;
+    height: 2em;
     margin-left: 10;
 		margin-right: 10;
   }
