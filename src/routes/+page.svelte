7b1d870e60abe751c9e9cb766b34913602091242
Luke <lmyszka44@gmail.com>
2023-11-28 19:19:01 -0600

    Fixed the landing page logo size and added additional App details

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <style>
-#text {
+#title {
 	text-align: center;
 	justify-content: center;
+	font-family: Proxima;
+	color: rgb(255, 255, 255);
+}
+#details {
+	text-align: center;
 	font-family: Proxima;
 	color: rgb(255, 255, 255);
 }
@@ -10,13 +15,25 @@
 <script lang="ts">
 </script>
 
-<div id="text">
-	<h1 style="font-size: 50px;">Compakt</h1>
+<div id="title">
+	<h1 style="font-size: 50px;">The Compakt Web App</h1>
 	<h1 style="font-size: 35px;">By:</h1>
 	<img
-    src="/multirotorlogo.png"
+    src="/Test.png"
     alt="logo"
-    width="70%"
+    width="50%"
     height="auto"
 />
+</div>
+
+<div id="details">
+<h1 style="font-size: 35px; margin-top:3ch">What is this for?</h1>
+<h1 style="font-size: 20px; margin-top:-1ch">The Compakt app is a tool designed by the Multirotor Software Division <br>
+	to assist with drone testing days by providing data logging and retrieval <br>
+	capabilities for flight data, among other useful features.</h1>
+<h1 style="font-size: 35px; margin-top:5ch">How is it used?</h1>
+<h1 style="font-size: 20px; margin-top:-1ch">There are two main pages you will need to navigate between, The Flight Recorder and the Flight Logs <br>
+	The Flight Recorder does exactly what the name suggests, that is where you will go to enter information and record your flight.<br>
+	Once a flight has been recorded, that information will be stored within the app, where it can be viewed in the Flight Logs. <br>
+	From there, you can view, manage, and export any previously recorded flight logs <br>
 </div>
