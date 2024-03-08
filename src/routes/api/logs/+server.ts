4288843943408bd17f304397293b92d29865f80a
tkbessler <tkbessler@gmail.com>
2024-03-07 19:31:49 -0600

    stringify response

--- src/routes/api/logs/+server.ts
+++ src/routes/api/logs/+server.ts
@@ -9,7 +9,7 @@ export const GET: RequestHandler = async (ev) => {
 
 	const results = await stmt.all();
 
-	return new Response(results);
+	return new Response(JSON.stringify(results));
 };
 
 export const POST: RequestHandler = async (ev) => {
