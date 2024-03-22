5620a97a3867946c02e2a9cb47d0dbde9c942fba
tkbessler <tkbessler@gmail.com>
2024-03-21 19:39:10 -0500

    separate out init function

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -4,5 +4,4 @@ import type { FlightData, FlightLog } from '$lib/structs';
 export const InfoVisible = writable(true);
 export const FlightRecord = writable({ initialized: false } as FlightData);
 export const LogArray = writable([] as FlightLog[]);
-export const Init = writable(false);
 export const FlagInvalid = writable(false);
