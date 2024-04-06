99160d57b41bfd580c4715cf1f81e019fd36261e
tkbessler <tkbessler@gmail.com>
2024-04-06 12:53:10 -0500

    off by one initial value

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -4,4 +4,4 @@ import type { FlightData, FlightLog } from '$lib/structs';
 export const FlightRecord = writable({ initialized: false } as FlightData);
 export const LogArray = writable([] as FlightLog[]);
 export const FlagInvalid = writable(false);
-export const ReservedId = writable(1);
+export const ReservedId = writable(0);
