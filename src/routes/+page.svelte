a79b164c411c38da6870982dc5c90998c82a41df
Luke <lmyszka44@gmail.com>
2024-04-16 20:06:28 -0500

    Adjusting About Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,20 +10,29 @@
 	<div id="details">
 		<h1 style="font-size: 35px; margin-top:4ch">How do I use Compakt?</h1>
 		<h1 style="font-size: 20px; margin-top:-1ch">
-			There are two main pages to navigate between, the "Record" and the "Logs" pages. <br>
-			Record does exactly what the name suggests, that is where you will go to enter information <br>
-			and record your flight. Once a flight has been recorded, the information will be used to create a record in our database <br> 
-			where it can be viewed via the logs page. The created logs are sorted by most recent to oldest by default <br>
-			and can be expanded to show more detailed weather data. You can also manage any previously recorded <br>
-			flight log with options for exporting or deleting flight logs. <br>
+			<div class="text-box-container">
+				<div class="text-box"> 
+					There are two main pages to navigate between, the "Record" and the "Logs" pages. <br>
+					Record does exactly what the name suggests, that is where you will go to enter information <br>
+					and record your flight. Once a flight has been recorded, the information will be used to create a record in our database <br> 
+					where it can be viewed via the logs page. Within the logs page, you can manage any previously recorded <br>
+					flight log with options for exporting or deleting flight logs. The created logs are sorted <br>
+					by most recent to oldest by default and can be expanded to show more detailed weather data. <br>
+				</div>
+			</div>
 		</h1>
 		<h1 style="font-size: 35px; margin-top:4ch">Key Information</h1>
 		<h1 style="font-size: 20px; margin-top:-1ch">
-			- Compakt is compatible with both mobile and desktop platforms that have a connection to the internet. <br>
-			- The weather API used to gather data is based on the general area of the city provided, not specifically the testing location <br>
-			- The database is updated in real time when a flight is recorded and will display the flight in the logs page <br> 
-			- Flight logs are exported as .CSV files from the logs page by selecting the logs you need and then clicking the download button <br>
-			- The proper authorities / personnel are required to be contacted and notified before the drone is flown <br>
+			<div class="text-box-container">
+				<div class="text-box"> 
+					- Compakt is compatible with both mobile and desktop platforms that have a connection to the internet. <br>
+					- The weather API used to gather data is based on the general area of the city provided, not specifically the testing location <br>
+					- After a flight is recorded, Compakt will update in real time to display the data in the logs page <br>
+					- Flight logs can be exported as .CSV files from the logs page by selecting the logs you need and then clicking the download button <br>
+					- The proper authorities / personnel are required to be contacted and notified before the drone is flown <br>
+					- All entry fields must be filled out in the record page in order for a log to be created.
+				</div>
+			</div>
 		</h1>
 	</div>
 </div>
@@ -43,5 +52,19 @@
 		text-align: center;
 		font-family: 'IBMPlexSans-Regular';
 		color: rgb(255, 255, 255);
+	}
+	.text-box {
+		border: 1px solid #000000;
+		margin-bottom: 0.25em;
+		padding: 1.5em;
+		padding-bottom: 0;
+		padding-top: 0;
+		text-align: center;
+		width: calc(100vw - 50em);
+	}
+	.text-box-container {
+		justify-content: center;
+		display: flex;
+
 	}
 </style>
